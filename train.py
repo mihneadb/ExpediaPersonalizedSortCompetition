6f941f7250e1ab48b5d4aca6d9d3a66aae0a9d5a
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2014-05-21 00:18:01 +0200

    Gradient boosting. 447

--- train.py
+++ train.py
@@ -1,12 +1,12 @@
 import data_io
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 
 def main():
     print("Reading training data")
     train = data_io.read_train()
     train.fillna(0, inplace=True)
 
-    train_sample = train.fillna(value=0)
+    train_sample = train[:100000].fillna(value=0)
 
     feature_names = list(train_sample.columns)
     feature_names.remove("click_bool")
@@ -15,18 +15,12 @@ def main():
     feature_names.remove("date_time")
     feature_names.remove("position")
 
-    feature_names.remove("orig_destination_distance")
-    feature_names.remove("srch_room_count")
-    feature_names.remove("srch_saturday_night_bool")
-
-    target_names = ["booking_bool", "click_bool"]
     features = train_sample[feature_names].values
-    target = train_sample[target_names].values
+    target = train_sample["booking_bool"].values
 
     print("Training the Classifier")
-    classifier = RandomForestClassifier(n_estimators=3200,
+    classifier = GradientBoostingClassifier(n_estimators=200,
                                         verbose=2,
-                                        n_jobs=1,
                                         min_samples_split=10,
                                         random_state=1)
     classifier.fit(features, target)
